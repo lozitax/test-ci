51f1825abf75d02408d9b531c8482662ffd32d37
maxime lemoine <maxime.lemoine76@gmail.com>
2023-06-06 11:36:04 +0200

    Modif

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <div className={styles.grid}>
           <Link href="/about" className={styles.card}>
             <h2>About Page &rarr;</h2>
-            <p>Cypress will test if this link is working.</p>
+            <p>testtesttest.</p>
           </Link>
 
           <a href="https://nextjs.org/docs" className={styles.card}>
